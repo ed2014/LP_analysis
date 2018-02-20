3423b7ce75250e5362ccf0eea463c046a229217e
PFR\cfleit <edmar.teixeira@plantandfood.co.nz>
2018-02-21 08:58:59 +1300

    Tidy up final graps for publication

--- Graph_kl_decayRates.R
+++ Graph_kl_decayRates.R
@@ -1,8 +1,8 @@
 library(tidyr)
 library(dplyr)
 library(ggplot2)
-round1Dir <- "\\\\Lindfs12\\home$\\cfleit\\My Documents\\My papers\\2013-LucernePasture\\kl\\submitted\\round_1" # for graphs
-
+#round1Dir <- "\\\\Lindfs12\\home$\\cfleit\\My Documents\\My papers\\2013-LucernePasture\\kl\\submitted\\round_1" # for graphs
+saveDir <- "C:\\GitHubRepos\\LP_analysis\\"
 #info <- read.table("\\\\Lindfs12\\home$\\Cfleit\\My Documents\\My papers\\2013-LucernePasture\\kl\\graphs\\kl_decayRates.txt", header=TRUE)
 
 info <- read.table("C:\\GitHubRepos\\LP_analysis\\kl_decayRates.txt", header=TRUE)
@@ -17,11 +17,11 @@ g <- info %>%
   geom_point() +
   ylab("Surface kl multiplier (fractional)") +
   xlab("Depth in soil profile (mm)")+
-  theme(text = element_text(size=10))+ 
+  theme(text = element_text(size=15))+ 
  # theme(legend.title=element_blank())+
   labs(colour=expression(lambda["kl"]))
 
 g
 
 # Save
-ggsave(file="Fig_5_kl_decay.tiff", dpi = 300, path=round1Dir, g)
+ggsave(file="Fig_5_kl_decay.tiff", dpi = 300, path=saveDir,width = 12, height = 6, g)
